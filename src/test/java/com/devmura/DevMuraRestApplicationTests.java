5a26d6061b7882cb7e66dc2def429b2a5d533347
David Vazquez <dave_u@outlook.com>
2023-05-26 15:21:01 -0600

    first commit

---
+++ src/test/java/com/devmura/DevMuraRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMuraRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
