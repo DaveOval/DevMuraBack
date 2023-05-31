a48f64dfdf2e49c167fb134cd1b7faa38d515591
Tenshik0 <tenshik0.ale@gmail.com>
2023-05-30 23:26:22 -0600

    countries-country

--- src/main/java/com/devmura/entity/Country.java
+++ src/main/java/com/devmura/entity/Country.java
@@ -1,11 +1,16 @@
 package com.devmura.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor  
 @Entity
 @Table(name = "countries")
-public class Countries {
+public class Country {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "country_id")
@@ -13,4 +18,6 @@ public class Countries {
 
     @Column(name = "country_name")
     private String name;
+
+
 }
