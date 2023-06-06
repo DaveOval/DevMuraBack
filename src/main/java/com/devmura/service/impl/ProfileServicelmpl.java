d4aace66f483d85a432439d417ac7dfbddc7ec11
Tenshik0 <tenshik0.ale@gmail.com>
2023-06-05 19:19:02 -0600

    ProfileServicelmpl

--- src/main/java/com/devmura/service/impl/ProfileServicelmpl.java
+++ src/main/java/com/devmura/service/impl/ProfileServicelmpl.java
@@ -1,8 +1,37 @@
 package com.devmura.service.impl;
 
+import com.devmura.entity.Profile;
+import com.devmura.repository.ProfileRepository;
+import com.devmura.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ProfileServicelmpl {
+import java.util.List;
+import java.util.Optional;
 
+@Service
+public class ProfileServicelmpl implements ProfileService {
+    @Autowired
+    ProfileRepository profileRepository;
+
+    @Override
+    public void save(Profile profile) {
+        profileRepository.save(profile);
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+        profileRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Profile> findProfileById(Integer id) {
+        return profileRepository.findById(id);
+    }
+
+    @Override
+    public List<Profile> getAll() {
+        return profileRepository.findAll();
+    }
 }
