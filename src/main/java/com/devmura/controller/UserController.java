4d1febe48eddc489bfe8960f19ee36b2f83f634a
David Vazquez <dave_u@outlook.com>
2023-06-08 16:23:55 -0600

    se agregaron las imagenes a los inserts

--- src/main/java/com/devmura/controller/UserController.java
+++ src/main/java/com/devmura/controller/UserController.java
@@ -53,7 +53,6 @@ public class UserController {
 
                     userService.save(user);
 
-
                     Profile profile = new Profile();
                     profile.setUser(user);
                     Optional<Level> level = levelService.findLevelById(2);
