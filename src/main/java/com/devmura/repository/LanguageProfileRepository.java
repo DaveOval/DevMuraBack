a28b6c936cbe80123e015f099a5780ce4194037c
Tenshik0 <tenshik0.ale@gmail.com>
2023-06-15 02:12:56 -0600

    Languages
    Se guardan pero muestra el ID de la tabla intermedia

---
+++ src/main/java/com/devmura/repository/LanguageProfileRepository.java
@@ -0,0 +1,8 @@
+package com.devmura.repository;
+
+import com.devmura.entity.LanguageProfileWR;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageProfileRepository extends JpaRepository<LanguageProfileWR,Integer> {
+
+}
