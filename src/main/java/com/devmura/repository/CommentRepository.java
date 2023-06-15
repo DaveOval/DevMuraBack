398165067ed2f20c5f8f6593695ef702987d874f
David Vazquez <dave_u@outlook.com>
2023-06-15 14:28:15 -0600

    se agrego la anotacion repository

--- src/main/java/com/devmura/repository/CommentRepository.java
+++ src/main/java/com/devmura/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.devmura.repository;
 
 import com.devmura.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }
