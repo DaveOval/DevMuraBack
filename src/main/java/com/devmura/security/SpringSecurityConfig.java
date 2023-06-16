ae5d56d7d30bfce025632e741fec990b8b89f0de
David Vazquez <dave_u@outlook.com>
2023-06-16 15:36:10 -0600

    awda

--- src/main/java/com/devmura/security/SpringSecurityConfig.java
+++ src/main/java/com/devmura/security/SpringSecurityConfig.java
@@ -63,7 +63,7 @@ public class SpringSecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedOrigins(Arrays.asList("https://devmuraback.up.railway.app/"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
